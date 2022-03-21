60273db60b8f9e76039bd39a51ae7255dd0bce42
anh nguyen <nguyenlamanh1701@gmail.com>
2022-03-21 13:43:44 +0700

    update db

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'lap_trinh_web');
+   define('DB_SERVER', 'us-cdbr-east-05.cleardb.net');
+   define('DB_USERNAME', 'ba55f238d969ac');
+   define('DB_PASSWORD', '543cdb02');
+   define('DB_DATABASE', 'heroku_18068c451b8ec16');
    $conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
