258765825740870384e3310a39bd108422c292d4
anh nguyen <nguyenlamanh1701@gmail.com>
2022-03-24 20:27:32 +0700

    update

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
-// define('DB_SERVER', 'us-cdbr-east-05.cleardb.net');
-// define('DB_USERNAME', 'bf8b06d1f27ab6');
-// define('DB_PASSWORD', '034ed77b');
-// define('DB_DATABASE', 'heroku_31fc207ed1cb063');
-// $conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-// mysqli_set_charset($conn, 'utf8mb4');
+define('DB_SERVER', 'us-cdbr-east-05.cleardb.net');
+define('DB_USERNAME', 'bf8b06d1f27ab6');
+define('DB_PASSWORD', '034ed77b');
+define('DB_DATABASE', 'heroku_31fc207ed1cb063');
+$conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+mysqli_set_charset($conn, 'utf8mb4');
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'lap_trinh_web');
-$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_DATABASE', 'lap_trinh_web');
+// $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 if (!$conn) {
    die('khong the ket noi database:');
